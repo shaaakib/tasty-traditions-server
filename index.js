604128a5608e42bd9b570c010f5c32ad53c896eb
Muhammad Shakib Hossain <shakibhossainmuhammad@gmail.com>
2023-05-02 20:28:58 +0600

    added recipe information

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const chef = require('./data/chef.json');
+
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Tasty Traditions is running');
+});
+
+app.get('/chef', (req, res) => {
+  res.send(chef);
+});
+
+app.get('/chef/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedChef = chef.find((n) => n.id === id);
+  res.send(selectedChef);
 });
 
 app.listen(port, () => {
